98e58f7fc962199ded076ed0615b859524943f0c
Partha Maity <parthmty@gmail.com>
2022-11-18 16:28:30 +0530

    ADD v1

--- src/scripts/main.ts
+++
@@ -1,9 +0,0 @@
-import controller from './controller';
-
-// User click Next and Prev
-controller.nextButton.addEventListener('click', () => {
-  controller.next();
-});
-controller.prevButton.addEventListener('click', () => {
-  controller.prev();
-});
--- src/scripts/steppers.ts
+++
@@ -1,6 +0,0 @@
-import s1 from './v1';
-import s2 from './v2';
-import s3 from './v3';
-import s4 from './v4';
-
-export default [s1, s2, s3, s4];
